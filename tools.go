e9bfde23800ca80b32857cc8988b00f593fd8e31
Stefan Pochmann <stefan.pochmann@gmail.com>
2015-02-14 16:31:56 +0100

    Added waitForEnter to tools.

--- tools.go
+++ tools.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strconv"
 )
 
 func getInt32(s string) int32 {
 	i, _ := strconv.ParseInt(s, 10, 32)
 	return int32(i)
+}
+
+func waitForEnter(message string) {
+	fmt.Print(message, " (waiting, hit Enter)")
+	bufio.NewReader(os.Stdin).ReadLine()
 }
